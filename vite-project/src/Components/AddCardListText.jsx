efe36072742bea46bff5e3ed5d7418d84de1af49
Brigitte Bolaños <brigittebedo@hotmail.com>
2024-07-11 16:21:56 -0500

    tarjeta lista comenza funcionalidad

--- vite-project/src/Components/AddCardListText.jsx
+++ vite-project/src/Components/AddCardListText.jsx
@@ -1,7 +1,7 @@
-import { Button, IconButton, InputBase, Paper } from '@mui/material'
+import { Box, Button, Fade, IconButton, InputBase, Paper } from '@mui/material'
 import React, { useState } from 'react'
 import { styled } from '@mui/system'
-import {Clear} from "@mui/icons-material"
+import ClearIcon from '@mui/icons-material/Clear';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 
 const AddCardListText = () => {
@@ -16,20 +16,27 @@ const AddCardListText = () => {
       placeholder='enter title'
       />
     </StyledPaper>
-    <div>
-      <Button>Add Card</Button>
-      <IconButton>
-        <Clear />
-      </IconButton>
-      <IconButton>
-        <MoreHorizIcon/>
-      </IconButton>
-    </div>
-    
-    
+    <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Box display="flex" alignItems="center">
+          <StyledButton>Add Card</StyledButton>
+          <IconButton>
+            <ClearIcon />
+          </IconButton>
+        </Box>
+        <Box flexGrow={1} />
+        <IconButton>
+          <MoreHorizIcon />
+        </IconButton>
+      </Box>
     </>
-  )
+  );
 }
+
+    
+    
+
+
+    
 const StyledPaper = styled(Paper)(({ theme }) => ({
   width: '280px',
   paddingBottom: theme.spacing(4),
@@ -42,5 +49,13 @@ const StyledInputBase = styled(InputBase)( ({theme}) => ({
 
   }
 }))
+const StyledButton = styled(Button)(({theme }) =>({
+  background: "#5aac44",
+  color: "#fff",
+  "&:hover":{
+    background: "#4e8a38",
+  }
+}))
+
 
 export default AddCardListText
